bd75b1f824a833fe547d0ca548264f1c7e3c6391
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-21 16:13:18 +0400

    Update pingallbot.py

--- pingallbot.py
+++ pingallbot.py
@@ -23,7 +23,7 @@ chatQueue = []
 
 stopProcess = False
 
-@teletips.on_message(filters.command(["ping","all"]))
+@teletips.on_message(filters.command(["ledytag"]))
 async def everyone(client, message):
   global stopProcess
   try: 
@@ -89,7 +89,7 @@ async def everyone(client, message):
   except FloodWait as e:
     await asyncio.sleep(e.value) 
 
-@teletips.on_message(filters.command(["remove","clean"]))
+@teletips.on_message(filters.command(["remove","sil"]))
 async def remove(client, message):
   global stopProcess
   try: 
@@ -230,36 +230,6 @@ async def bots(client, message):
   except FloodWait as e:
     await asyncio.sleep(e.value)
 
-@teletips.on_message(filters.command("start") & filters.private)
-async def start(client, message):
-  text = f'''
-Heya {message.from_user.mention},
-My name is **PingAll**. I'm here to help you to get everyone's attention by mentioning all members in your chat.
-
-I have some additional cool features and also I can work in channels.
-
-Don't forget to join my [channel](http://t.me/teletipsofficialchannel) to recieve information on all the latest updates.
-
-Hit /help to find out my commands and the use of them.
-'''
-  await teletips.send_message(message.chat.id, text, disable_web_page_preview=True)
-
-
-@teletips.on_message(filters.command("help"))
-async def help(client, message):
-  text = '''
-Hey, let's have a quick look at my commands.
-
-**Commands**:
-- /ping "input": <i>Mention all members.</i>
-- /remove: <i>Remove all deleted accounts.</i>
-- /admins: <i>Mention all admins.</i>
-- /bots: <i>Get the full bot list.</i>
-- /stop: <i>Stop an on going process.</i>
-
-If you have any questions on how to use me, feel free to ask in my [support group](https://t.me/teletipsofficialontopicchat). More on my [page](https://github.com/teletips/PingAllBot-TeLeTiPs).
-'''
-  await teletips.send_message(message.chat.id, text, disable_web_page_preview=True)
 
 print("PingAll is alive!")  
 teletips.run()
